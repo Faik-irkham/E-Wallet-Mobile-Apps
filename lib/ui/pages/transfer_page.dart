dd078967a98c0e335d771feef4908c97494105c2
faik-irkham <faikirkham@gmail.com>
2023-06-07 21:01:32 +0700

    transfer result user item

--- lib/ui/pages/transfer_page.dart
+++ lib/ui/pages/transfer_page.dart
@@ -1,6 +1,8 @@
 import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/widgets/buttons.dart';
 import 'package:bank_sha/ui/widgets/forms.dart';
 import 'package:bank_sha/ui/widgets/transfer_recent_user_item.dart';
+import 'package:bank_sha/ui/widgets/transfer_result_user_item.dart';
 import 'package:flutter/material.dart';
 
 class TransferPage extends StatelessWidget {
@@ -24,11 +26,18 @@ class TransferPage extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 14),
-          CustomFormField(
+          const CustomFormField(
             title: 'by username',
             isShowTitle: false,
           ),
-          buildRecentUsers(),
+          // buildRecentUsers(),
+          buildResult(),
+          const SizedBox(height: 274),
+          CustomFilledButton(
+            title: 'Continue',
+            onPressed: () {},
+          ),
+          const SizedBox(height: 50),
         ],
       ),
     );
@@ -65,6 +74,44 @@ class TransferPage extends StatelessWidget {
             username: 'yoenna',
             isVerified: true,
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildResult() {
+    return Container(
+      margin: const EdgeInsets.only(top: 40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Result',
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semiBold,
+            ),
+          ),
+          const SizedBox(height: 14),
+          Wrap(
+            spacing: 17,
+            runSpacing: 17,
+            children: const [
+              TransferResultUserItem(
+                imageUrl: 'assets/img_friend1.png',
+                name: 'Yonna Jie',
+                username: 'yoenna',
+                isVerified: true,
+              ),
+              TransferResultUserItem(
+                imageUrl: 'assets/img_friend2.png',
+                name: 'Yonne Ka',
+                username: 'yoenyu',
+                isVerified: true,
+                isSelected: true,
+              ),
+            ],
+          )
         ],
       ),
     );
