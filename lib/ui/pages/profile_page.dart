c1dbf01587c33e654c18ae7fd85953459e9521ba
faik-irkham <faikirkham@gmail.com>
2023-07-02 03:24:25 +0700

    ganti data user menjadi dinamis

--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -35,111 +35,124 @@ class ProfilePage extends StatelessWidget {
             );
           }
 
-          return ListView(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            children: [
-              const SizedBox(height: 30),
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 30,
-                  vertical: 22,
-                ),
-                decoration: BoxDecoration(
-                  color: whiteColor,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Column(
-                  children: [
-                    Container(
-                      width: 120,
-                      height: 120,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          image: AssetImage('assets/img_profile.png'),
-                        ),
-                      ),
-                      child: Align(
-                        alignment: Alignment.topRight,
-                        child: Container(
-                          width: 28,
-                          height: 28,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: whiteColor,
-                          ),
-                          child: Center(
-                            child: Icon(
-                              Icons.check_circle,
-                              color: greenColor,
-                              size: 24,
-                            ),
+          if (state is AuthSuccess) {
+            return ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              children: [
+                const SizedBox(height: 30),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 30,
+                    vertical: 22,
+                  ),
+                  decoration: BoxDecoration(
+                    color: whiteColor,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 120,
+                        height: 120,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: state.user.profilePicture == null
+                                ? const AssetImage(
+                                    'assets/img_profile.png',
+                                  )
+                                : NetworkImage(
+                                    state.user.profilePicture!,
+                                  ) as ImageProvider,
                           ),
                         ),
+                        child: state.user.verified == 1
+                            ? Align(
+                                alignment: Alignment.topRight,
+                                child: Container(
+                                  width: 28,
+                                  height: 28,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: whiteColor,
+                                  ),
+                                  child: Center(
+                                    child: Icon(
+                                      Icons.check_circle,
+                                      color: greenColor,
+                                      size: 24,
+                                    ),
+                                  ),
+                                ),
+                              )
+                            : null,
                       ),
-                    ),
-                    const SizedBox(height: 16),
-                    Text(
-                      'Shayna Hanna',
-                      style: blackTextStyle.copyWith(
-                        fontSize: 18,
-                        fontWeight: medium,
+                      const SizedBox(height: 16),
+                      Text(
+                        state.user.name.toString(),
+                        style: blackTextStyle.copyWith(
+                          fontSize: 18,
+                          fontWeight: medium,
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 40),
-                    ProfileMenuItem(
-                      iconUrl: 'assets/ic_edit_profile.png',
-                      title: 'Edit Profile',
-                      onTap: () async {
-                        if (await Navigator.pushNamed(context, '/pin') ==
-                            true) {
-                          Navigator.pushNamed(context, '/profile-edit');
-                        }
-                      },
-                    ),
-                    ProfileMenuItem(
-                      iconUrl: 'assets/ic_pin.png',
-                      title: 'My PIN',
-                      onTap: () async {
-                        if (await Navigator.pushNamed(context, '/pin') ==
-                            true) {
-                          Navigator.pushNamed(context, '/profile-edit-pin');
-                        }
-                      },
-                    ),
-                    ProfileMenuItem(
-                      iconUrl: 'assets/ic_wallet.png',
-                      title: 'Wallet Setting',
-                      onTap: () {},
-                    ),
-                    ProfileMenuItem(
-                      iconUrl: 'assets/ic_reward.png',
-                      title: 'My Rewards',
-                      onTap: () {},
-                    ),
-                    ProfileMenuItem(
-                      iconUrl: 'assets/ic_help.png',
-                      title: 'Help Center',
-                      onTap: () {},
-                    ),
-                    ProfileMenuItem(
-                      iconUrl: 'assets/ic_logout.png',
-                      title: 'Log Out',
-                      onTap: () {
-                        context.read<AuthBloc>().add(AuthLogout());
-                      },
-                    ),
-                  ],
+                      const SizedBox(height: 40),
+                      ProfileMenuItem(
+                        iconUrl: 'assets/ic_edit_profile.png',
+                        title: 'Edit Profile',
+                        onTap: () async {
+                          if (await Navigator.pushNamed(context, '/pin') ==
+                              true) {
+                            Navigator.pushNamed(context, '/profile-edit');
+                          }
+                        },
+                      ),
+                      ProfileMenuItem(
+                        iconUrl: 'assets/ic_pin.png',
+                        title: 'My PIN',
+                        onTap: () async {
+                          if (await Navigator.pushNamed(context, '/pin') ==
+                              true) {
+                            Navigator.pushNamed(context, '/profile-edit-pin');
+                          }
+                        },
+                      ),
+                      ProfileMenuItem(
+                        iconUrl: 'assets/ic_wallet.png',
+                        title: 'Wallet Setting',
+                        onTap: () {},
+                      ),
+                      ProfileMenuItem(
+                        iconUrl: 'assets/ic_reward.png',
+                        title: 'My Rewards',
+                        onTap: () {},
+                      ),
+                      ProfileMenuItem(
+                        iconUrl: 'assets/ic_help.png',
+                        title: 'Help Center',
+                        onTap: () {},
+                      ),
+                      ProfileMenuItem(
+                        iconUrl: 'assets/ic_logout.png',
+                        title: 'Log Out',
+                        onTap: () {
+                          context.read<AuthBloc>().add(AuthLogout());
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(height: 87),
-              CustomTextButton(
-                title: 'Report a Problem',
-                onPressed: () {},
-              ),
-              const SizedBox(height: 50),
-            ],
-          );
+                const SizedBox(height: 87),
+                CustomTextButton(
+                  title: 'Report a Problem',
+                  onPressed: () {},
+                ),
+                const SizedBox(height: 50),
+              ],
+            );
+          }
+
+          return Container();
         },
       ),
     );
